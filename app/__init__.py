8949c40e463a98b54be4462157904b5c4b1a2317
andre002wp <andre002wp@gmail.com>
2022-06-06 03:33:05 +0800

    database and data done

--- app/__init__.py
+++ app/__init__.py
@@ -9,22 +9,22 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from app import routes
+from app.model import masterCategory
+from app.model import subCategory
 from app.model import articleType
+from app.model import role
+from app.model import user
+from app.model import kategori
+from app.model import tahunKeluaran
+from app.model import style
+from app.model import genderBaju
+from app.model import musim
+from app.model import outfit
+from app.model import size
+from app.model import foto_outfit
+
 from app.model import detailOrder
 from app.model import faq
-from app.model import foto_outfit
-from app.model import genderBaju
-from app.model import jenisOutfit
-from app.model import kategori
-from app.model import masterCategory
-from app.model import musim
 from app.model import orderan
 from app.model import review
-from app.model import role
-from app.model import size
-from app.model import style
-from app.model import subCategory
-from app.model import tahunKeluaran
-from app.model import user
-from app.model import outfit
 
