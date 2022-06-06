00497f7579b2089a6e85a2d32043fb256f274e2a
andre002wp <andre002wp@gmail.com>
2022-06-06 23:34:05 +0800

    Update database-data.sql

--- ML-model/database-data.sql
+++ ML-model/database-data.sql
@@ -12,7 +12,10 @@ MySQL - 10.4.11-MariaDB-log : Database - db_capfits
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-USE `db_capfits`;
+/*Data for the table `alembic_version` */
+
+insert  into `alembic_version`(`version_num`) values 
+('f6b50103bca0');
 
 /*Data for the table `article_type` */
 
